479141a783c686f317a02f988aa0f95641618f1f
yamil564 <sas523363.fsf@hotmail.com>
2020-10-13 17:00:39 -0500

    commit 3

--- src/main/java/com/tencsup/lab04/controller/UserController.java
+++ src/main/java/com/tencsup/lab04/controller/UserController.java
@@ -1,6 +1,29 @@
 package com.tencsup.lab04.controller;
+import com.tencsup.lab04.model.*;
+import java.util.Scanner;
 
 public class UserController {
 	
 	
+	public static void main(String args[]) {
+		String nom;
+		String ape;
+		int eda;
+		User x= new User();
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Nombre: ");
+		nom = teclado.nextLine();
+		System.out.print("Apellidos: ");
+		ape = teclado.nextLine();
+		System.out.print("Edad: ");
+		eda = teclado.nextInt();
+		
+		x.nombre=nom;
+		x.apellidos=ape;
+		x.edad=eda;
+		System.out.println("Nombre del usuario: "+x.nombre);
+		System.out.println("Apellidos del usuario: "+x.apellidos);
+		System.out.println("Edad del usuario: "+x.edad);
+	}
 }
--- src/main/java/com/tencsup/lab04/model/User.java
+++ src/main/java/com/tencsup/lab04/model/User.java
@@ -1,5 +1,7 @@
 package com.tencsup.lab04.model;
 
 public class User {
-
+	public String nombre;
+	public String apellidos;
+	public int edad;
 }
