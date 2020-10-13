585e71a3875402322f0932ab7da345672bed3fab
Ivan <Ivan@DESKTOP-VIBOMF8>
2020-10-13 17:17:35 -0500

    Cambio de Ivan

--- src/main/java/com/tencsup/lab04/util/MyString.java
+++ src/main/java/com/tencsup/lab04/util/MyString.java
@@ -1,5 +1,10 @@
 package com.tencsup.lab04.util;
 
 public class MyString {
-
+	public static void main(String[] args) {
+		String lenguaje = "HOLA MUNDO"; 
+		
+		String convertir = lenguaje.toLowerCase();
+		System.out.println(convertir);
+	}
 }
